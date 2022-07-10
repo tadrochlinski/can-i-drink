92eec6eed61316551c033288c5d972ee3428e630
tadrochlinski <tadrochlinski@gmail.com>
2022-07-10 16:21:38 +0200

    css done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.scss';
+import { useState } from 'react';
 
-function App() {
+const App = () =>{
+  
+
   return (
     <div className="App">
-      <h1>
-        Hello World
-      </h1>
+      <h1 className="title">can i drink?</h1>
+      <form className="form">
+        <input type="number" className="form__num-input" placeholder='enter your age'/>
+        <button className="button">Check!</button>
+      </form>
+      <div className="output"></div>
     </div>
   );
 }
