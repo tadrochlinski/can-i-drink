9a1f5caf512c3f1b7eea4895f332c92ef6333505
tadrochlinski <tadrochlinski@gmail.com>
2022-07-10 16:37:56 +0200

    logic created

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import './App.scss';
 import { useState } from 'react';
 
 const App = () =>{
-  
-
+  const [age, setAge] = useState(Number());
+  const [outputContent, setOutputContent] = useState('')
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    
+    if(age < 18) setOutputContent('No 👶')
+    if(age < 25) setOutputContent('Not in the USA 😇')
+    if(age >= 25) setOutputContent('Yes 🍻')
+  }
   return (
     <div className="App">
       <h1 className="title">can i drink?</h1>
-      <form className="form">
-        <input type="number" className="form__num-input" placeholder='enter your age'/>
+      <form className="form" onSubmit={handleSubmit}>
+        <input type="number" className="form__num-input" placeholder='enter your age' value={age} onChange = {(e) => setAge(e.target.value)}/>
         <button className="button">Check!</button>
       </form>
-      <div className="output"></div>
+      <div className="output">{outputContent}</div>
     </div>
   );
 }
